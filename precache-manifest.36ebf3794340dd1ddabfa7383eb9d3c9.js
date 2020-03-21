d8921963998736438bc8c01694a9ad7eeaf9f375
divcraft <t.ogonowski@wp.pl>
2020-03-21 14:27:00 +0100

    Updates

---
+++ precache-manifest.36ebf3794340dd1ddabfa7383eb9d3c9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c48e012e6736dce4f29e981fea40520",
+    "url": "/design-me/index.html"
+  },
+  {
+    "revision": "0f1a0126a8288feebb2a",
+    "url": "/design-me/static/css/main.7c5a0202.chunk.css"
+  },
+  {
+    "revision": "d93e9528e1367a502572",
+    "url": "/design-me/static/js/2.bdfe4837.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/design-me/static/js/2.bdfe4837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1a0126a8288feebb2a",
+    "url": "/design-me/static/js/main.a3e5a473.chunk.js"
+  },
+  {
+    "revision": "4d4e291bb0e99a3193bb",
+    "url": "/design-me/static/js/runtime-main.534b0dd2.js"
+  },
+  {
+    "revision": "88a6c3f4eb06f0d803326be8e341a68e",
+    "url": "/design-me/static/media/banner-1010.88a6c3f4.jpg"
+  },
+  {
+    "revision": "a6bdae40e20b921149fd51c5fb8c9bb6",
+    "url": "/design-me/static/media/banner-1366.a6bdae40.jpg"
+  },
+  {
+    "revision": "c4825517332990d5abe37a03d2ad7642",
+    "url": "/design-me/static/media/banner-1920.c4825517.jpg"
+  },
+  {
+    "revision": "7b57c52650dcde5fb0dfa23281d27831",
+    "url": "/design-me/static/media/contact-img-768.7b57c526.jpg"
+  },
+  {
+    "revision": "13d184667a024718a7024845aa5f261d",
+    "url": "/design-me/static/media/home-img1-1366.13d18466.jpg"
+  },
+  {
+    "revision": "30710895c1e24b1e22ba1d5b6629c67c",
+    "url": "/design-me/static/media/home-img1-1920.30710895.jpg"
+  },
+  {
+    "revision": "43d58b9ea34badd478ebd6e120f865ad",
+    "url": "/design-me/static/media/home-img1-768.43d58b9e.jpg"
+  },
+  {
+    "revision": "a9eadad23296b0187fdd851d6cc091fc",
+    "url": "/design-me/static/media/home-img2-1366.a9eadad2.jpg"
+  },
+  {
+    "revision": "55bf5c10c8cbcef94109d579c093c4a1",
+    "url": "/design-me/static/media/home-img2-1920.55bf5c10.jpg"
+  },
+  {
+    "revision": "74a7ade10274136045a9649423037ad9",
+    "url": "/design-me/static/media/home-img2-768.74a7ade1.jpg"
+  },
+  {
+    "revision": "05c483aa812b2e5a1aebb3722aa7dc41",
+    "url": "/design-me/static/media/illustration1-768.05c483aa.jpg"
+  },
+  {
+    "revision": "535735dd2dc12250f1fd5bd6a4f32172",
+    "url": "/design-me/static/media/illustration2-768.535735dd.jpg"
+  },
+  {
+    "revision": "d43833284f3d9d15402c92160b1e88df",
+    "url": "/design-me/static/media/illustration3-768.d4383328.jpg"
+  },
+  {
+    "revision": "07e5855a6a899f0493e1522709876e9b",
+    "url": "/design-me/static/media/illustration4-768.07e5855a.jpg"
+  },
+  {
+    "revision": "cbb4f47becba093c68bdbae3e2b33efe",
+    "url": "/design-me/static/media/illustration5-768.cbb4f47b.jpg"
+  },
+  {
+    "revision": "ad07224646a051f7c8641ff7d62865dc",
+    "url": "/design-me/static/media/illustration6-768.ad072246.jpg"
+  },
+  {
+    "revision": "467b8adf19f2b63445a528bab8e5c277",
+    "url": "/design-me/static/media/li-underline.467b8adf.svg"
+  },
+  {
+    "revision": "fcaf83cc20526f751cbaf15f713397a4",
+    "url": "/design-me/static/media/man-img.fcaf83cc.jpg"
+  },
+  {
+    "revision": "7c65ac32b240a6dc7b8871008b2abf40",
+    "url": "/design-me/static/media/menu-icon.7c65ac32.svg"
+  }
+]);
