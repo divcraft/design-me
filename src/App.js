90cc0f397d369a9422811e629b7c0aaf985662a1
divcraft <t.ogonowski@wp.pl>
2020-03-21 17:35:02 +0100

    add ScrollToTop.js component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/style.sass';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import HomePage from './pages/HomePage';
 import StrategyPage from './pages/StrategyPage';
 import FAQPage from './pages/FAQPage';
@@ -9,13 +10,15 @@ import ContactPage from './pages/ContactPage';
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route path='/' exact component={HomePage} />
-        <Route path='/strategy' component={StrategyPage} />
-        <Route path='/faq' component={FAQPage} />
-        <Route path='/contact' component={ContactPage} />
-        <Redirect to='/' />
-      </Switch>
+      <ScrollToTop>
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/strategy' component={StrategyPage} />
+          <Route path='/faq' component={FAQPage} />
+          <Route path='/contact' component={ContactPage} />
+          <Redirect to='/' />
+        </Switch>
+      </ScrollToTop>
     </Router>
   );
 }
